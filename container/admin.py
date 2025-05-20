0b9bb4fc01babc9b13c229311cb6fba78460a6bf
Vadim <vadim.marakushin@gmail.com>
2025-05-20 08:01:47 +0300

    add admin

--- container/admin.py
+++ container/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from container.models import Container
+
+
+@admin.register(Container)
+class ContainerAdmin(admin.ModelAdmin):
+    list_display = ('number', 'manufacture_year', 'status_hydrotesting', 'volume', 'client')
+    search_fields = ('number', 'client')
